df13ef0345f06224ddb5b3a5bfbeb8e487f11cd6
BerezovskaMaria <berez060910@gmail.com>
2024-05-16 01:31:12 +0300

    done

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'BerezovskaMaria', // Usually your GitHub org/user name.
-  projectName: 'BerezovskaMaria.github.io', // Usually your repo name.
+  projectName: 'BerezovskaMaria', // Usually your repo name.
   deploymentBranch: 'main',
   trailingSlash: false,
 
