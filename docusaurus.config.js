3b8af30810b04962760c924b2ef9a79c136954bf
BerezovskaMaria <berez060910@gmail.com>
2024-05-16 10:21:11 +0300

    yey

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: '← тут крокодил сидить',
+        title: '← тута крокодил сидить',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
